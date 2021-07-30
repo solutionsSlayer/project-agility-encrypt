b9044970302f116b2fbae352a1acc529b6901c60
Thoogui <guillaumefix33@gmail.com>
2021-07-30 10:46:15 +0200

    added swagger

--- server/app.js
+++ server/app.js
@@ -2,9 +2,19 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const YML = require('yaml')
+const fs = require('fs');
+const swaggerDocumentYml = YML.parse(fs.readFileSync('./swagger.yml', 'utf-8'));
+console.log(swaggerDocumentYml)
 //Import router;
 const cryptRouter = require('./routes/crypt.router');
+var options = {
+    swaggerOptions: {
+        url: 'http://petstore.swagger.io/v2/swagger.json'
+    }
+}
 
 const app = express();
 
@@ -17,6 +27,8 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors());
 
 app.use('/crypt', cryptRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentYml));
+
 
 app.listen(port, () => {
 });
--- server/controllers/crypt.controller.js
+++ server/controllers/crypt.controller.js
@@ -3,7 +3,7 @@ const hexToString = require("../utils/hexToString");
 
 exports.encrypt = (req, res, next) => {
     if (req.body.text && req.body.key) {
-        res.status(200).json(CryptoJS.AES.encrypt(req.body.text, req.body.key).toString());
+        res.status(200).json({ EncryptedReturn: CryptoJS.AES.encrypt(req.body.text, req.body.key).toString()});
     } else {
         res.status(400).json({'error': `Can't add, bad fields. Check the documentation`});
     }
@@ -11,7 +11,7 @@ exports.encrypt = (req, res, next) => {
 
 exports.decrypt = (req, res, next) => {
     if (req.body.text && req.body.key) {
-        res.status(200).json(hexToString.hexToString(CryptoJS.DES.decrypt(text, key).toString()));
+        res.status(200).json({ DecryptedReturn: hexToString.hexToString(CryptoJS.DES.decrypt(text, key).toString())});
     } else {
         res.status(400).json({'error': `Can't add, bad fields. Check the documentation`});
     }
