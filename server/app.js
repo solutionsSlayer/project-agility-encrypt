1f932a0012c43d79d25a138da5609b719a1f2321
RsMan-Dev <wily.richeroffi@gmail.com>
2021-07-30 11:36:04 +0200

    swagger fixed

--- server/app.js
+++ server/app.js
@@ -7,14 +7,9 @@ const swaggerDocument = require('./swagger.json');
 const YML = require('yaml')
 const fs = require('fs');
 const swaggerDocumentYml = YML.parse(fs.readFileSync('./swagger.yml', 'utf-8'));
-console.log(swaggerDocumentYml)
 //Import router;
 const cryptRouter = require('./routes/crypt.router');
-var options = {
-    swaggerOptions: {
-        url: 'http://petstore.swagger.io/v2/swagger.json'
-    }
-}
+
 
 const app = express();
 
--- server/controllers/crypt.controller.js
+++ server/controllers/crypt.controller.js
@@ -1,18 +1,35 @@
 const CryptoJS = require('crypto-js');
 const hexToString = require("../utils/hexToString");
+const utf8 = require("utf8/utf8");
+const algorithms = ["AES", "DES", "TripleDES", "Rabbit", "RC4", "RC4Drop"]
 
 exports.encrypt = (req, res, next) => {
-    if (req.body.text && req.body.key) {
-        res.status(200).json({ EncryptedReturn: CryptoJS.AES.encrypt(req.body.text, req.body.key).toString()});
+    if (req.body.text && req.body.key && req.body.algorithm) {
+        if(algorithms.includes(req.body.algorithm)){
+            res.status(200).json({ encrypted: CryptoJS.AES.encrypt(req.body.text, req.body.key).toString()});
+        } else {
+            res.status(422).json({error: `wrong algotithm, allowed: ${algorithms.join(", ")}`});
+        }
     } else {
-        res.status(400).json({'error': `Can't add, bad fields. Check the documentation`});
+        res.status(422).json({error: `Cannot process with this data`});
     }
 }
 
 exports.decrypt = (req, res, next) => {
-    if (req.body.text && req.body.key) {
-        res.status(200).json({ DecryptedReturn: hexToString.hexToString(CryptoJS.DES.decrypt(text, key).toString())});
+    if (req.body.text && req.body.key && req.body.algorithm) {
+        if(algorithms.includes(req.body.algorithm)){
+            let resultUtf8;
+            try{
+                resultUtf8 = utf8.decode(hexToString(CryptoJS.AES.decrypt(req.body.text, req.body.key).toString()));
+            }catch(e){
+                resultUtf8 = "cannot decrypt";
+            }
+            if(!resultUtf8) resultUtf8 =  "error, wrong key or algorithm"
+            res.status(200).json({ decrypted: resultUtf8});
+        } else {
+            res.status(422).json({error: `wrong algotithm, allowed: ${algorithms.join(", ")}`});
+        }
     } else {
-        res.status(400).json({'error': `Can't add, bad fields. Check the documentation`});
+        res.status(422).json({error: `Cannot process with this data`});
     }
 }
